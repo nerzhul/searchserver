a6af5485f0c28635263146e3e1f446576ac6a8d9
Loic Blot <loic.blot@unix-experience.fr>
2016-02-20 21:27:57 +0100

    Go searchserver: implement JSON parsing

--- searchserver.go
+++ searchserver.go
@@ -2,17 +2,69 @@ package main
 
 import (
 	"net/http"
+	"io/ioutil"
+	"encoding/json"
 
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/gzip"
 	"github.com/martini-contrib/render"
 )
 
-func search(r render.Render) {
+type SearchRequest struct {
+	Request string `json:"s"`
+	Page uint32 `json:"p"`
+}
+
+func search(r render.Render, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		r.JSON(500, map[string]interface{}{})
+		return
+	}
+
+	var t SearchRequest
+	err = json.Unmarshal(body, &t)
+	if err != nil {
+		r.JSON(500, map[string]interface{}{})
+		return
+	}
+
+	// Empty requests or nil pages , or page greater than 100 are not allowed
+	if len(t.Request) == 0 || t.Page == 0 || t.Page > 100 {
+		r.JSON(500, map[string]interface{}{})
+		return
+	}
+
 	r.JSON(200, map[string]interface{}{"remote_engine": map[string]interface{}{}, "exact": map[string]interface{}{}})
 }
 
-func markInterest(r render.Render) {
+type InterestRequest struct {
+	Url string `json:"url"`
+	Searched_Terms string `json:"terms_searched"`
+	Content string `json:"content"`
+	Title string `json:"title"`
+}
+
+func markInterest(r render.Render, req *http.Request) {
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		r.JSON(500, map[string]interface{}{})
+		return
+	}
+
+	var t InterestRequest
+	err = json.Unmarshal(body, &t)
+	if err != nil {
+		r.JSON(500, map[string]interface{}{})
+		return
+	}
+
+	// Empty requests or nil pages , or page greater than 100 are not allowed
+	if len(t.Url) == 0 || len(t.Searched_Terms) == 0 || len(t.Content) == 0 || len(t.Title) == 0 {
+		r.JSON(500, map[string]interface{}{})
+		return
+	}
+
 	r.JSON(200, map[string]interface{}{"ok": true})
 }
 
